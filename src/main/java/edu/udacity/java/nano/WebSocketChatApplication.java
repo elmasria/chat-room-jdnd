04ec3f7b38b870d8dd289e39749ec0ffe51efd28
Ahmad El Masri <ahmadjelmasri@gmail.com>
2019-10-12 16:54:46 +0200

    feat: add code for chat room backend login
    get the username and build web socket url

--- src/main/java/edu/udacity/java/nano/WebSocketChatApplication.java
+++ src/main/java/edu/udacity/java/nano/WebSocketChatApplication.java
@@ -30,7 +30,9 @@ public class WebSocketChatApplication {
      */
     @GetMapping("/index")
     public ModelAndView index(String username, HttpServletRequest request) throws UnknownHostException {
-        //TODO: add code for login to chatroom.
-        return null;
+        ModelAndView chatModelView = new ModelAndView("chat");
+        chatModelView.addObject("webSocketUrl", "ws://localhost:"+request.getLocalPort()+"/chat");
+        chatModelView.addObject("username", username );
+        return chatModelView;
     }
 }
