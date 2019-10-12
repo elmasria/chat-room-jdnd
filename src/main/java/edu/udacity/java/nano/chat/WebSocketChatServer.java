26425b84aa6553aad440119a1bd502e67cfb1ba2
Ahmad El Masri <ahmadjelmasri@gmail.com>
2019-10-12 17:33:51 +0200

    feat: add send message functionality

--- src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
+++ src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -1,7 +1,6 @@
 package edu.udacity.java.nano.chat;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
@@ -54,6 +53,10 @@ public class WebSocketChatServer {
     @OnMessage
     public void onMessage(Session session, String jsonStr) {
         //TODO: add send message.
+        Message msgObject = JSON.parseObject(jsonStr, Message.class);
+        msgObject.setType(MessageType.SPEAK);
+        String message = JSON.toJSON(msgObject).toString();
+        sendMessageToAll(message);
     }
 
     /**
